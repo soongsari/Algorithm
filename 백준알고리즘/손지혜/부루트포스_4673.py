cbe991439201a499720a4b12d0560ae366cc8686
손지혜 <sonjeehe@gmail.com>
2022-05-08 20:52:54 +0900

    소스 최적화

--- 백준알고리즘/손지혜/부루트포스_4673.py
+++ 백준알고리즘/손지혜/부루트포스_4673.py
@@ -1,18 +1,17 @@
 
-arr=[0]*12000
+arr=[0]*10001
 
 def d(n):
 
     if(n> 10000):
         return
     val = n + sum(list(map(int,str(n)))) #자릿수 더하기
-    if(arr[val]==1): #중복된 숫자는 더이상 확인하지 않음
+    if(val > 10000 or arr[val]==1): #중복된 숫자는 더이상 확인하지 않음
         return
     arr[val]=1 #계산 된 결과가 있는 건에 1표시
     return d(val)
 
 i = 1
-j = 1
 while(i<=10000):
     d(i)
     if arr[i]!=1:
