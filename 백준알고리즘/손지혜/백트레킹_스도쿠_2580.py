4aec019978975ff6e566aea5a2e78af39e1e6c22
soongsari <sonjeehe@gmail.com>
2022-06-03 20:31:37 +0900

    백트레킹 스토쿠문제 추가

--- 백준알고리즘/손지혜/백트레킹_스도쿠_2580.py
+++ 백준알고리즘/손지혜/백트레킹_스도쿠_2580.py
@@ -18,12 +18,12 @@ for i in range(3) :
     sqr.append(temp)
 
 for i in range(9):
-    sudoku.append(list(map(int,sys.stdin.readline().split(" "))))
+    sudoku.append(list(map(int,sys.stdin.readline().rstrip().split(" "))))
 
     for j in range(9):
         val = sudoku[i][j]
         if val == 0:
-            blank.append(str(i)+" " +str(j))
+            blank.append(str(i)+str(j))
         else:
             row[i].remove(val)
             col[j].remove(val)
@@ -31,26 +31,32 @@ for i in range(9):
 
 
 
-def bfs(slist):
-    if len(blank)==0:
+def bfs(index,slist):
+
+
+    if index == len(blank):
         for r in slist:
             print(' '.join(map(str, r)))
         exit()
 
-    for i in range(len(blank)):
-        val = blank.pop(0)
-        x = int(val.split(" ")[0])
-        y = int(val.split(" ")[1])
+    
+    val = blank[index]
+    x = int(val[0])
+    y = int(val[1])
 
-        for j in (set(row[x]) & set(col[y]) & set(sqr[x//3][y//3])):
-            slist[x][y]=j
-            row[x].remove(j)
-            col[y].remove(j)
-            sqr[x//3][y//3].remove(j)
-            bfs(slist)
-            row[x].append(j)
-            col[y].append(j)
-            sqr[x//3][y//3].append(j)
-            blank.append(val)
+    interset = (set(row[x]) & set(col[y]) & set(sqr[x//3][y//3]))
+
+
+    for j in interset:
+        slist[x][y]=j
+        row[x].remove(j)
+        col[y].remove(j)
+        sqr[x//3][y//3].remove(j)
+        bfs(index+1,slist)
+        slist[x][y]=0
+        row[x].append(j)
+        col[y].append(j)
+        sqr[x//3][y//3].append(j)
+
             
-bfs(sudoku)
+bfs(0,sudoku)
